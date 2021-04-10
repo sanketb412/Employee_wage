1b9114eddf22704f18c66105f9d125f053dbb530
Shweta Singh <Shweta.Singh1@Nykaa.local>
2021-04-10 10:15:30 +0530

    Added: code of Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,4 +13,22 @@ else
 	echo "Employee is Absent"
 fi
 
+#UC2
+
+isFullTime=1
+empRatePerHr=20
+empCheck=$((RANDOM%2));
+
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+
+salary=$(($empHrs*$empRatePerHr));
+
+
 
