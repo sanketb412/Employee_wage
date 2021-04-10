379eb900485570cd17d941724b6a0468c358e45a
Shweta Singh <Shweta.Singh1@Nykaa.local>
2021-04-10 18:43:53 +0530

    Added: code for Monthly Wages for 20 Days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,31 +9,26 @@ randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ];
 then
 	echo "Employee is Present"
+
+	#UC2
+	isFullTime=1;
+	isPartTime=2;
+	empRatePerHr=20;
+	empCheck=$((RANDOM%3));
+
+	case $empCheck in
+		$isFullTime)
+			empHrs=8
+			;;
+		$isPartTIme)
+			empHrs=4
+			;;
+	esac
+
+	salary=$(($empHrs*$empRatePerHr*20));
+
+	echo $salary
+
 else
-	echo "Employee is Absent"
+        echo "Employee is Absent"
 fi
-
-#UC2
-
-isFullTime=1;
-isPartTime=2;
-empRatePerHr=20;
-empCheck=$((RANDOM%3));
-
-case $empCheck in
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTIme)
-		empHrs=4
-		;;
-	*)
-	empHrs=0
-		;;
-esac
-
-salary=$(($empHrs*$empRatePerHr));
-
-echo $salary
-
-
